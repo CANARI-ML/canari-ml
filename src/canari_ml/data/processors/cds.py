847dfdba186fbf7fe53b6141980b4c1eff980dce
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2025-04-22 15:38:13 +0100

    feat(preprocess): Add ERA5 zg compute and preprocessing

---
+++ src/canari_ml/data/processors/cds.py
@@ -0,0 +1,34 @@
+import logging
+
+from preprocess_toolbox.processor import NormalisingChannelProcessor
+
+
+class ERA5PreProcessor(NormalisingChannelProcessor):
+    def pre_normalisation(self, var_name: str, da: object):
+        if 'expver' in da.coords:
+            logging.warning("expvers {} in coordinates, will process out but "
+                            "this needs further work: expver needs storing for "
+                            "later overwriting".format(da.expver))
+
+        # Checking if variable is geopotential, like: 'zg250', 'zg500'
+        # i.e., zg + an integer number
+        is_geopotential_var = False
+        if var_name.startswith("zg"):
+            is_geopotential_var = "".join(var_name.split("zg")).isdigit()
+
+        if var_name == "tos":
+            logging.debug("ERA5 regrid postprocessing replacing zeroes: {}".format(var_name))
+            da = da.fillna(0)
+        elif is_geopotential_var:
+            # Convert from geopotential to geopotential height
+            logging.debug("ERA5 additional regrid: {}".format(var_name))
+            da /= 9.80665
+
+        return da
+
+    def post_normalisation(self, var_name: str, da: object):
+        ## TODO: Redundant, refactor this in future
+        logging.info("Renaming ERA5 spatial coordinates to match SIC")
+        if "x" in da.coords and "y" in da.coords:
+            da = da.rename(dict(x="xc", y="yc"))
+        return da

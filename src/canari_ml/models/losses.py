ad5a0f930cc141c4e977305085b187c4bfbf73fc
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2025-04-23 17:13:27 +0100

    refactor(loss): Update dimension shifting, commented loss plotting code

--- src/canari_ml/models/losses.py
+++ src/canari_ml/models/losses.py
@@ -16,11 +16,11 @@ class L1Loss(nn.L1Loss):
 
         # Computing using nn.L1Loss class. This class must be instantiated via:
         # >>> criterion = WeightedL1Loss(reduction="none")
-        loss = super().forward(y_hat.movedim(-2, 1), targets.movedim(-1, 1)) * sample_weights.movedim(-1, 1)
+        loss = super().forward(y_hat.movedim(-1, 1), targets.movedim(-1, 1)) * sample_weights.movedim(-1, 1)
 
         # Computing here, in the derived class
         # loss = (
-        #             torch.abs( ( y_hat.movedim(-2, 1) - targets.movedim(-1, 1) )*100 )
+        #             torch.abs( ( y_hat.movedim(-1, 1) - targets.movedim(-1, 1) )*100 )
         #         )*sample_weights.movedim(-1, 1)
 
         return loss.mean()
@@ -31,6 +31,7 @@ class MSELoss(nn.MSELoss):
         if "reduction" not in kwargs:
             kwargs.update({"reduction": "none"})
         super().__init__(*args, **kwargs)
+        self.count = 1
 
     def forward(self, inputs, targets, sample_weights):
         """
@@ -39,15 +40,60 @@ class MSELoss(nn.MSELoss):
         Compute MSE loss weighted by masking.
 
         """
-        y_hat = inputs
+        inputs, targets, sample_weights = inputs, targets, sample_weights
+        print("inputs.shape:", inputs.shape)
+        print("targets.shape:", targets.shape)
+        print("sample_weights.shape:", sample_weights.shape)
+        y_hat = inputs.movedim(-2, 1).movedim(-1, 1)
+        targets = targets.movedim(-1, 1).movedim(-1, 1)
+        sample_weights = sample_weights.movedim(-1, 1).movedim(-1, 1)
+        print("inputs.shape:", y_hat.shape)
+        print("targets.shape:", targets.shape)
+        print("sample_weights.shape:", sample_weights.shape)
+        #exit()
+
+        y_hat = y_hat * sample_weights
+        targets = targets * sample_weights
+
+        #if self.count % 100 == 0:
+        #    import matplotlib.pyplot as plt
+        #    #img1 = y_hat[0, :, :, 0, 0].cpu().data.numpy()
+        #    #img2 = targets[0, :, :, 0, 0].cpu().data.numpy()
+        #    img1 = y_hat[0, 0, 0, :, :].cpu().data.numpy()
+        #    img2 = targets[0, 0, 0, :, :].cpu().data.numpy()
+        #    print("img1 shape:", img1.shape)
+        #    print("img2 shape:", img2.shape)
+        #    fig, axes = plt.subplots(2, 2, figsize=(12, 6), layout="constrained")
+        #    # Prediction at leadtime 0
+        #    ## Plot current prediction
+        #    im1 = axes[0, 0].imshow(img1, cmap="RdBu_r")
+        #    plt.colorbar(im1, ax=axes[0, 0])
+        #    axes[0, 0].set_title("y_hat: leadtime 0")
+        #    ## Plot target
+        #    im2 = axes[1, 0].imshow(img2, cmap="RdBu_r")
+        #    plt.colorbar(im2, ax=axes[1, 0])
+        #    axes[1, 0].set_title("target: leadtime 0")
+        #    # Prediction at last leadtime
+        #    img1 = y_hat[0, -1, 0, :, :].cpu().data.numpy()
+        #    img2 = targets[0, -1, 0, :, :].cpu().data.numpy()
+        #    ## Plot current prediction
+        #    im1 = axes[0, 1].imshow(img1, cmap="RdBu_r")
+        #    plt.colorbar(im1, ax=axes[0, 1])
+        #    axes[0, 1].set_title("y_hat: last leadtime")
+        #    ## Plot target
+        #    im2 = axes[1, 1].imshow(img2, cmap="RdBu_r")
+        #    plt.colorbar(im2, ax=axes[1, 1])
+        #    axes[1, 1].set_title("target: last leadtime")
+        #    plt.show()
+        #self.count += 1
 
         # Computing using nn.MSELoss base class. This class must be instantiated via:
         # criterion = nn.MSELoss(reduction="none")
-        loss = super().forward(y_hat.movedim(-2, 1), targets.movedim(-1, 1)) * sample_weights.movedim(-1, 1)
+        loss = super().forward(y_hat, targets)
 
         # Computing here, in the nn.Module derived class
         # loss = (
-        #             ( ( y_hat.movedim(-2, 1) - targets.movedim(-1, 1) )*100 )**2
+        #             ( ( y_hat.movedim(-1, 1) - targets.movedim(-1, 1) )*100 )**2
         #         )*sample_weights.movedim(-1, 1)
         return loss.mean()
 
@@ -64,7 +110,7 @@ class HuberLoss(nn.HuberLoss):
         Compute Huber loss weighted by masking.
 
         """
-        y_hat = inputs.movedim(-2, 1) * sample_weights.movedim(-1, 1)
+        y_hat = inputs.movedim(-1, 1) * sample_weights.movedim(-1, 1)
         targets = targets.movedim(-1, 1) * sample_weights.movedim(-1, 1)
         return super().forward(y_hat, targets).mean()
 

e1b400e7172c53ce59b5594c79fab05a6767e71b
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2025-04-29 14:10:46 +0100

    fix(unet): Re-enable padding for varied dimension support
    Needed to enable this for 500x500 input grid, and 0.3
    n_filters_factor.

--- src/canari_ml/models/models.py
+++ src/canari_ml/models/models.py
@@ -162,12 +162,12 @@ class UNet(nn.Module):
         stride = max_pool_kernel_size**n_max_pool
 
         h, w = x.shape[-2:]
-        ## Pad data if the input image dimensions is not a multiple of stride
-        #if h % stride or w % stride:
-        #    padding = get_padding(x, stride=stride)
-        #    x = apply_padding(x, padding)
-        #else:
-        #    padding = None
+        # Pad data if the input image dimensions is not a multiple of stride
+        if h % stride or w % stride:
+           padding = get_padding(x, stride=stride)
+           x = apply_padding(x, padding)
+        else:
+           padding = None
 
         # Encoder
         bn1 = self.conv1(x)
@@ -198,9 +198,9 @@ class UNet(nn.Module):
         # Final layer
         output = self.final_layer(up9)
 
-        ## Undo padding of the network output to recover the original input shape
-        #if padding:
-        #    output = undo_padding(output, padding)
+        # Undo padding of the network output to recover the original input shape
+        if padding:
+           output = undo_padding(output, padding)
 
         # Convert raw logits to result
         # Can do without since we're working with regression w/ continuous values

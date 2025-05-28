07bc4936e5a944bfece06af1cc8e4df8ddc31239
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2025-05-28 01:44:03 +0100

    docs(litmodule): Add docstrings to Classes
    Refs: #28

--- src/canari_ml/models/model_wrapper.py
+++ src/canari_ml/models/model_wrapper.py
@@ -12,9 +12,9 @@ class BaseLightningModule(pl.LightningModule):
     Base class for all Canari ML models using PyTorch Lightning.
 
     This module inherits from `pytorch_lightning.LightningModule` and provides the basic
-    functionality required for training, validating, and testing Canari ML models. It also
-    includes support for saving hyperparameters to checkpoints and recording metrics during
-    training and validation.
+    functionality required for training, validating, and testing Canari ML models. It
+    also includes support for saving hyperparameters to checkpoints and recording
+    metrics during training and validation.
 
     Attributes:
         model (nn.Module): The PyTorch model being wrapped.
@@ -22,8 +22,8 @@ class BaseLightningModule(pl.LightningModule):
         learning_rate (float): The learning rate used for optimisation.
         metrics (Iterable[callable]): An iterable of callable objects representing the
             metrics to be recorded during training and validation.
-        enable_leadtime_metrics (bool, optional): Flag indicating whether to enable lead-time
-            related metrics. Defaults to True.
+        enable_leadtime_metrics (bool, optional): Flag indicating whether to enable
+            lead-time related metrics. Defaults to True.
         n_output_classes (int): The number of output classes in the model.
     """
     def __init__(
@@ -71,24 +71,31 @@ class BaseLightningModule(pl.LightningModule):
         self.test_metrics = metric_collection.clone(prefix="test_")
 
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
         Implement forward function.
-        :param x: Inputs to model.
-        :return: Outputs of model.
+
+        Applies the model to the input tensor `x`.
+
+        Args:
+            x: Inputs to the model. Expected shape is
+                `(batch_size, channels, height, width)`.
+
+        Returns:
+            Output of the model with shape `(batch_size, num_classes, height, width)`.
         """
         return self.model(x)
 
 
-    def on_save_checkpoint(self, checkpoint):
+    def on_save_checkpoint(self, checkpoint: dict) -> None:
         """
         Override PyTorch Lightning's default `on_save_checkpoint` method to add custom data.
 
-        This method adds the name of the class and the path to the Lightning module to the
-        checkpoint.
+        This method adds the name of the class and the path to the Lightning module to
+        the checkpoint.
 
         Args:
-            checkpoint (dict): The checkpoint dictionary to which additional data will be added.
+            checkpoint: The checkpoint dictionary to which additional data will be added.
         """
         # Add name of class and path to the lightning module to checkpoint
         # TODO: Add code version/git commit tag to it as well
@@ -97,29 +104,41 @@ class BaseLightningModule(pl.LightningModule):
 
 
 class LitUNet(BaseLightningModule):
+    r"""
+    A LightningModule wrapping the :class:`UNet` implementation of IceNet.
     """
-    A LightningModule wrapping the UNet implementation of IceNet.
-    """
-
     def __init__(self, *args, **kwargs):
-        """
-        Construct a UNet LightningModule.
-        Note that we keep hyperparameters separate from dataloaders to prevent data leakage at test time.
-        :param model: PyTorch model
-        :param criterion: PyTorch loss function for training instantiated with reduction="none"
-        :param learning_rate: Float learning rate for our optimiser
-        """
         super().__init__(*args, **kwargs)
 
 
-    def training_step(self, batch, batch_idx):
-        """
+    def training_step(self, batch: dict, batch_idx: int) -> dict:
+        r"""
         Perform a pass through a batch of training data.
-        Apply pixel-weighted loss by manually reducing.
-        See e.g. https://discuss.pytorch.org/t/unet-pixel-wise-weighted-loss-function/46689/5.
-        :param batch: Batch of input, output, weight triplets
-        :param batch_idx: Index of batch
-        :return: Loss from this batch of data for use in backprop
+
+        This method implements the core training loop for a single batch of data.
+        It takes the input, output, and sample weights from the provided batch,
+        passes the inputs through the model to obtain predictions, computes
+        the pixel-weighted loss using the provided criterion, and updates any
+        relevant metrics. The computed loss is then returned for use in backpropagation.
+
+        Args:
+            batch: A dictionary containing 'x', 'y', and 'sample_weights'
+                keys with their respective values representing input data,
+                target output data, and sample weights.
+            batch_idx: Index of the current batch.
+
+        Returns:
+            A dictionary containing the computed loss for this batch
+                of data. This is used in backpropagation to update the model's
+                parameters.
+
+        .. note::
+            The method uses pixel-weighted loss by manually reducing it, following the
+            approach outlined
+            `here <https://discuss.pytorch.org/t/unet-pixel-wise-weighted-loss-function/46689/5>`_.
+
+            It also logs the computed loss and metrics for use in monitoring training
+            progress.
         """
         x, y, sample_weight = batch["x"], batch["y"], batch["sample_weights"]
         outputs = self.model(x)
@@ -147,7 +166,24 @@ class LitUNet(BaseLightningModule):
 
         return {"loss": loss}
 
-    def validation_step(self, batch, batch_idx):
+    def validation_step(self, batch: dict, batch_idx: int) -> dict:
+        r"""
+        Perform a pass through a batch of validation data.
+
+        This method implements the core validation loop for a single batch of data.
+        The methodology is the same as `training_step`. The computed loss is logged
+        for use in monitoring validation progress.
+
+        Args:
+            batch: A dictionary containing 'x', 'y', and 'sample_weights'
+                keys with their respective values representing input data,
+                target output data, and sample weights.
+            batch_idx: Index of the current batch.
+
+        Returns:
+            dict: A dictionary containing the computed loss for this batch
+                of data. This is used in logging to monitor validation progress.
+        """
         # x: (b, h, w, channels), y: (b, h, w, lead_time, classes), sample_weight: (b, h, w, lead_time, classes)
         x, y, sample_weight = batch["x"], batch["y"], batch["sample_weights"]
         outputs = self.model(x)
@@ -174,7 +210,24 @@ class LitUNet(BaseLightningModule):
         )  # epoch-level metrics
         return {"val_loss": loss}
 
-    def test_step(self, batch, batch_idx):
+    def test_step(self, batch: dict, batch_idx: int) -> dict:
+        r"""
+        Perform a pass through a batch of test data.
+
+        This method implements the core testing loop for a single batch of data.
+        The methodology is the same as `training_step`. The computed loss is logged
+        for use in monitoring test progress.
+
+        Args:
+            batch: A dictionary containing 'x', 'y', and 'sample_weights'
+                keys with their respective values representing input data,
+                target output data, and sample weights.
+            batch_idx: Index of the current batch.
+
+        Returns:
+            A dictionary containing the computed loss for this batch
+                of data. This is used in logging to monitor test progress.
+        """
         x, y, sample_weight = batch["x"], batch["y"], batch["sample_weights"]
         outputs = self.model(x)
         y_hat = outputs
@@ -203,10 +256,21 @@ class LitUNet(BaseLightningModule):
         )  # epoch-level metrics
         return {"test_loss": loss}
 
-    def on_train_epoch_end(self):
-        """
-        Reference lightning v2.0.0 migration guide:
-        https://github.com/Lightning-AI/pytorch-lightning/pull/16520
+    def on_train_epoch_end(self) -> None:
+        r"""
+        Perform actions at the end of each training epoch.
+
+        This method is called by PyTorch Lightning at the end of each training epoch.
+        It computes and stores the average loss for the completed epoch, then resets
+        the metrics computed during individual training steps in preparation for the
+        next epoch.
+
+        .. note::
+            The implementation follows the migration guide from Lightning v1.5 to v2.0,
+            as outlined `here <https://github.com/Lightning-AI/pytorch-lightning/pull/16520>`_.
+
+            It also references `this issue <https://github.com/Lightning-AI/pytorch-lightning/issues/13147#issuecomment-1138975446>`_
+            for accessing logged results.
         """
         # Reference: https://github.com/Lightning-AI/pytorch-lightning/issues/13147#issuecomment-1138975446
         avg_train_loss = self.trainer.callback_metrics["loss"]
@@ -215,10 +279,14 @@ class LitUNet(BaseLightningModule):
         # Reset metrics computed in each training step
         self.train_metrics.reset()
 
-    def on_validation_epoch_end(self):
-        """
-        Reference lightning v2.0.0 migration guide:
-        https://github.com/Lightning-AI/pytorch-lightning/pull/16520
+    def on_validation_epoch_end(self) -> None:
+        r"""
+        Perform actions at the end of each validation epoch.
+
+        This method is called by PyTorch Lightning at the end of each validation epoch.
+        It computes and stores the average loss for the completed epoch, then updates,
+        stores, and resets the metrics computed during individual validation steps in
+        preparation for the next epoch.
         """
         avg_val_loss = self.trainer.callback_metrics["val_loss"]
         self.metrics_history["val_loss"].append(avg_val_loss.item())
@@ -231,24 +299,57 @@ class LitUNet(BaseLightningModule):
 
         self.val_metrics.reset()
 
-    def on_test_epoch_end(self):
+    def on_test_epoch_end(self) -> None:
+        """
+        Perform actions at the end of each test epoch.
+
+        This method is called by PyTorch Lightning at the end of each test epoch.
+        It logs and resets the metrics computed during individual test steps in
+        preparation for the next epoch.
+        """
         self.log_dict(
             self.test_metrics.compute(), on_step=False, on_epoch=True, sync_dist=True
         )  # epoch-level metrics
         self.test_metrics.reset()
 
-    def predict_step(self, batch, batch_idx):
+    def predict_step(self, batch: dict, batch_idx: int) -> torch.Tensor:
         """
-        :param batch: Batch of input, output, weight triplets
-        :param batch_idx: Index of batch
-        :return: Predictions for given input.
+        Generate predictions for a given input batch.
+
+        This method is called by PyTorch Lightning during prediction to generate model
+        outputs for the provided input batch. It returns the model's predictions based
+        on the input data.
+
+        Args:
+            batch: A dictionary containing the input ('x') and output ('y')
+                tensors, as well as any additional relevant information like
+                'sample_weights'.
+            batch_idx: The index of the current batch in the dataloader.
+
+        Returns:
+            y_hat: The model's predictions for the given input batch.
         """
         x, y, sample_weight = batch["x"], batch["y"], batch["sample_weights"]
         y_hat = self.model(x)
 
         return y_hat
 
-    def configure_optimizers(self):
+    def configure_optimizers(self) -> dict:
+        r"""
+        Configure and return the optimizer and learning rate scheduler.
+
+        This method is called by PyTorch Lightning to initialise the optimizer and
+        learning rate scheduler used for training the model. It returns a dictionary
+        containing both the optimizer and the lr_scheduler.
+
+        Returns:
+            A dictionary containing the optimizer and lr_scheduler.
+                - optimizer (torch.optim.optimizer.Optimizer): The optimizer instance
+                    used to update model parameters during training.
+                - lr_scheduler (torch.optim.lr_scheduler._LRScheduler): The learning
+                    rate scheduler instance, which adjusts the learning rate over time
+                    based on specified criteria.
+        """
         optimizer = torch.optim.AdamW(
             self.parameters(), lr=self.learning_rate, weight_decay=1e-4
         )

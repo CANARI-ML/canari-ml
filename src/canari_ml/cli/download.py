7a7b3bbfee301e62bdfc9798f6957895c48cf55d
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2025-06-24 17:44:21 +0100

    feat(hydra): Initial manual logging
    Refs: #43

--- src/canari_ml/cli/download.py
+++ src/canari_ml/cli/download.py
@@ -1,3 +1,4 @@
+import sys
 from datetime import datetime as dt
 from pathlib import Path
 
@@ -11,60 +12,87 @@ from .utils import run_command
 @hydra.main(
     version_base=None,
     config_path=str(Path(__file__).parent / "../../../conf"),
-    config_name="config"
+    config_name="config",
 )
-def main(cfg):
-    print(OmegaConf.to_yaml(cfg))
+def download(cfg):
+    Path("logs").mkdir(exist_ok=True)
+    now = dt.now()
+    log_file = f"logs/download.{now:%Y-%m-%d_%H-%M-%S}.log"
 
-    start_date = dt.strptime(cfg.download.date_range.start, "%Y-%m-%d").date()
-    end_date = dt.strptime(cfg.download.date_range.end, "%Y-%m-%d").date()
-    variables = ",".join(cfg.download.variables)
-    levels = ",".join(cfg.download.levels)
+    cfg_yaml = OmegaConf.to_yaml(cfg)
 
-    base_command = [
-        "download_aws",
-        "--config-path",
-        cfg.download.output_config_path,
-        "-f",
-        cfg.download.frequency,
-        "-o",
-        cfg.download.output_group_by,
-        "--workers",
-        str(cfg.download_params.workers),
-        cfg.download.hemisphere,
-    ]
+    print("HYDRA Configuration YAML:\n")
+    print(cfg_yaml)
 
-    if cfg.download_params.overwrite_config:
-        base_command.append("--overwrite-config")
+    with open(log_file, "w") as f:
+        f.write("HYDRA Configuration YAML:\n\n")
+        f.write(cfg_yaml)
+        f.write("\n\n")
+        start_date = dt.strptime(cfg.download.date_range.start, "%Y-%m-%d").date()
+        end_date = dt.strptime(cfg.download.date_range.end, "%Y-%m-%d").date()
+        variables = ",".join(cfg.download.variables)
+        levels = ",".join(cfg.download.levels)
 
-    if cfg.download_params.delete_cache:
-        base_command.append("--delete-cache")
+        base_command = [
+            "download_aws",
+            "--config-path",
+            cfg.download.output_config_path,
+            "-f",
+            cfg.download.frequency,
+            "-o",
+            cfg.download.output_group_by,
+            "--workers",
+            str(cfg.download_params.workers),
+            cfg.download.hemisphere,
+        ]
 
-    if cfg.download_params.cache_only:
-        base_command.append("--cache-only")
+        if cfg.download_params.overwrite_config:
+            base_command.append("--overwrite-config")
 
-    current_start_date = start_date
+        if cfg.download_params.delete_cache:
+            base_command.append("--delete-cache")
 
-    while current_start_date <= end_date:
-        command = base_command.copy()
-        current_end_date = current_start_date + relativedelta(day=31)
+        if cfg.download_params.cache_only:
+            base_command.append("--cache-only")
 
-        # Make sure not to outside of bounds
-        if current_end_date > end_date:
-            current_end_date = end_date
+        current_start_date = start_date
 
-        month_start_date = current_start_date.strftime("%Y-%m-%d")
-        month_end_date = current_end_date.strftime("%Y-%m-%d")
+        while current_start_date <= end_date:
+            command = base_command.copy()
+            current_end_date = current_start_date + relativedelta(day=31)
 
-        command.extend(
-            [month_start_date, month_end_date, f"'{variables}'", f"'{levels}'"]
-        )
+            # Make sure not to go outside of bounds
+            if current_end_date > end_date:
+                current_end_date = end_date
 
-        current_start_date += relativedelta(months=1)
+            month_start_date = current_start_date.strftime("%Y-%m-%d")
+            month_end_date = current_end_date.strftime("%Y-%m-%d")
 
-        run_command(command)
+            command.extend(
+                [month_start_date, month_end_date, f"'{variables}'", f"'{levels}'"]
+            )
 
-    print("All downloads completed.")
+            current_start_date += relativedelta(months=1)
+
+            run_command(command, log_file=f)
+
+        end_msg = "All downloads completed."
+        f.write(end_msg)
+        f.write("\n\n")
+
+        print(end_msg)
+
+
+def main():
+    # Disable HYDRA logging for downloader script
+    # sys.argv.append("hydra/hydra_logging=none")
+
+    # Disable HYDRA output directory creation
+    # sys.argv.append("hydra.output_subdir=null")
+
+    # sys.argv.append("hydra.run.dir='outputs/download'")
+    # sys.argv.append("hydra.output_subdir='logs'")
+    download()
 
 
 if __name__ == "__main__":
--- src/canari_ml/cli/utils.py
+++ src/canari_ml/cli/utils.py
@@ -1,6 +1,16 @@
 import subprocess
 
-def run_command(command):
+
+def run_command(command, log_file=None):
     command = [str(v) for v in command]
-    print(f"Running command: {' '.join(command)}")
-    subprocess.run(command)
+    cmd_str = f"Running command: {' '.join(command)}\n{'_' * 75}\n\n"
+    print(cmd_str)
+    if log_file:
+        log_file.write(cmd_str)
+    process = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+    )
+    for line in process.stdout:  # type: ignore
+        print(line, end="")
+        if log_file:
+            log_file.write(line)

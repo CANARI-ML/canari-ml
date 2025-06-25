4ae20c7c80edd575b7802a5c376a753df19fca0e
Bryn N. Ubald <55503826+bnubald@users.noreply.github.com>
2025-06-25 11:31:30 +0100

    chore(hydra): subprocess cmd now returns error

--- src/canari_ml/cli/utils.py
+++ src/canari_ml/cli/utils.py
@@ -5,11 +5,17 @@ def run_command(command):
     logger = logging.getLogger(__name__)
 
     command = [str(v) for v in command]
-    cmd_str = f"Running command: {' '.join(command)}\n{'_' * 75}\n\n"
+    cmd_str = f"\n\nRunning command: {' '.join(command)}\n{'_' * 75}\n\n"
     logger.info(cmd_str)
+
 
     process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
     )
     for line in process.stdout:  # type: ignore
         logger.info(line.strip())
+
+    process.wait()
+
+    if process.returncode != 0:
+        raise RuntimeError("Command failed with exit code %d" % process.returncode)
